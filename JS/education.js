a67f690c01aca6b6d38a8a5419d9fbfcaac3c7a8
jvarjun <67821028+jvarjun@users.noreply.github.com>
2020-07-04 13:32:59 +0530

    Add files via upload

---
+++ JS/education.js
@@ -0,0 +1,7 @@
+//--------------------------sidenavbar----------------------------//
+function openSideNav(){
+	document.getElementById("sidenav").style.display="block";
+}
+function closeSideNav(){
+	document.getElementById("sidenav").style.display="none";
+}
---
+++ JS/index.js
@@ -0,0 +1,11 @@
+//--------------------------sidenavbar----------------------------//
+function openSideNav(){
+	document.getElementById("sidenav").style.display="block";
+}
+function closeSideNav(){
+	document.getElementById("sidenav").style.display="none";
+}
+//-------------------------imageclick---------------------------//
+function opennew(){
+	window.location="personal info.html"
+}
